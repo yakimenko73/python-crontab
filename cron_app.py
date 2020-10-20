56cdbd2fe6b2c951ba25a3b6915fe4c20be2319b
Roman <53937692+yakimenko73@users.noreply.github.com>
2020-10-20 17:23:32 +0300

    Update cron_app.py

--- cron_app.py
+++ cron_app.py
@@ -1,3 +1,5 @@
+import os
+
 from crontab import CronTab
 from croniter import croniter
 
@@ -5,36 +7,65 @@ from datetime import datetime
 import time
 import pause
 import subprocess
+import psutil
+import signal
+
+def get_crontab(path):
+	crontab = CronTab(tabfile=PATH_TO_CRONTAB)
+	return crontab
 
 
-def get_list_jobs(crontab):
-	list_jobs = []
-	for job in crontab:
-		list_jobs.append(job)
-	return list_jobs
+class GracefulKiller:
+	kill_now = False
+	def __init__(self):
+		signal.signal(signal.SIGINT, self.exit_gracefully)
+		signal.signal(signal.SIGTERM, self.exit_gracefully)
 
+	def exit_gracefully(self,signum, frame):
+		self.kill_now = True
 
 if __name__ == "__main__":
 	PATH_TO_CRONTAB = '/home/roman/Python-works/Simcord/Cron/mycron.tab'
 
-	crontab = CronTab(tabfile=PATH_TO_CRONTAB)
+	crontab = get_crontab(PATH_TO_CRONTAB)
 
 	flag = True
 
-	print(get_list_jobs(crontab))
+	for i in range(len(crontab)):
+		pid = os.fork()
+		id_job = i
+		if pid == 0:
+			break
 
-	while flag:
-		t = datetime.today()
-		base = datetime(t.year, t.month, t.day, t.hour, t.minute, t.second)
+	print(pid, os.getpid())
 
-		iter_ = croniter(str(get_list_jobs(crontab)[0].slices), 
-							base)
-		task = iter_.get_next(datetime)
+	if pid == 0:
+		while flag:
+			time.sleep(1)
+			killer = GracefulKiller()
 
-		print(str(task), datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+			crontab = get_crontab(PATH_TO_CRONTAB)
+			
+			t = datetime.today()
+			base = datetime(t.year, 
+							t.month, 
+							t.day, 
+							t.hour, 
+							t.minute, 
+							t.second)
 
-		pause.until(task)
+			iter_ = croniter(str(crontab[id_job].slices),
+									base)
+			task = iter_.get_next(datetime)
 
-		print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Complete')
+			print(str(task), datetime.now().strftime("%Y-%m-%d %H:%M:%S"), id_job)
 
-		flag = True
+			pause.until(task)
+
+			status = os.system(str(crontab[id_job].command))
+			print(f'Task - {id_job}' + ' completed')
+
+			if killer.kill_now:
+				flag = False
+			else:
+				flag = True
