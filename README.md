d363e48f0dd2fe035652d9bdc5f250309987dcf0
yakimenko <r.yakimenko.73@gmail.com>
2020-12-17 12:39:19 +0200

    removed the ability to recursively run the cron_app.py script in crontab.tab

--- README.md
+++
@@ -1,4 +0,0 @@
-# SharedCron
-two authors: yakimenko73, Mysslim
-
-Implementation of the Cron algorithm in conjunction with Mysslim(vodyanov)
--- cron_app.py
+++ cron_app.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 import logging
 import configparser
 
@@ -12,12 +14,51 @@ from croniter import croniter
 
 from datetime import datetime
 
-config = configparser.ConfigParser()
-config.read("settings.ini")
 
-logging.basicConfig(filename = config["Path"]["PATH_TO_LOG"], filemode='a', level=logging.DEBUG,
+config = configparser.ConfigParser()
+config.read("settings/settings.ini")
+
+pidfile = "temp/server.pid"
+
+logging.basicConfig(filename = config["Path"]["PATH_TO_LOG"], 
+					filemode='a', 
+					level=logging.DEBUG,
 					format = '%(asctime)s.%(msecs)03d %(name)s %(levelname)s: %(message)s',
 					datefmt = '%Y-%m-%d %H:%M:%S')
+
+
+def is_process_started(pid):
+	""" Проверка запущен ли этот скрипт """
+	try:
+		os.kill(pid, 0)
+		return True
+	except OSError:
+		return False
+
+
+def check_pid():
+	""" Проверка является ли pid запущенного скрипта уже активным процессом """
+	if os.path.isfile(pidfile):
+		logging.info("Is there a file with a pid? - True")
+		with open(pidfile, "r") as f:
+			# случай, если файл server.pid пустой
+			try:
+				pid = int(f.read())
+				logging.info("The file is not empty? - True")
+			except ValueError as ex:
+				logging.warning(f"Exception: {ex}")
+				return
+
+		if is_process_started(pid):
+			logging.info("Is the script already running? - True")
+			try:
+				proc = psutil.Process(os.getpid())
+				proc.kill()
+			except SystemExit:
+				logging.warning('SystemExit')
+				os._exit(0)
+
+		logging.info("Is the script already running? - False")
 
 
 def get_crontab(path):
@@ -45,7 +86,6 @@ def work(id_job, path, crontab):
 							t.hour, 
 							t.minute, 
 							t.second)
-
 		try:
 			iter_ = croniter(str(crontab[id_job].slices), base)
 		except Exception:
@@ -54,11 +94,12 @@ def work(id_job, path, crontab):
 			job = ''
 			for i in crontab[3]:
 				job += str(i) + ' '
+
 			iter_ = croniter(job, base)
 
-		task = iter_.get_next(datetime)
+		task_time = iter_.get_next(datetime)
 
-		pause.until(task)
+		pause.until(task_time)
 
 		status = os.system(str(crontab[id_job].command))
 		logging.info(f'Task: {id_job}' + ' completed' + f' PID: {os.getpid()}')
@@ -79,13 +120,24 @@ def main(path):
 
 
 if __name__ == "__main__":
+	check_pid()
+
+	# при каждом запуске скрипта будет записываться его pid,
+	# это нужно для того чтобы исключить возможность добавить
+	# этот же файл для исполнения в crontab
+	pid = str(os.getpid())
+	with open(pidfile, "w") as f:
+		f.write(pid)
+
 	try:
 		main(config["Path"]["PATH_TO_CRONTAB"])
 	except KeyboardInterrupt:
-		logging.warning('KeyboardInterrupt')
+		logging.warning('KeyboardInterrupt' + f' PID: {os.getpid()}')
 		try:
 			proc = psutil.Process(os.getpid())
 			proc.kill()
 		except SystemExit:
 			logging.warning('SystemExit')
 			os._exit(0)
+	finally:
+		os.unlink(pidfile)
